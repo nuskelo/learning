971b759396194d8e73e84d9fcdd2c1ca1266c3d3
Vlad Oleksyn <nuskelo.official@gmail.com>
2026-05-30 18:35:33 +0300

    feat: practice LINQ orderby, descending and Reverse methods

--- LinqPractice/Program.cs
+++ LinqPractice/Program.cs
@@ -2,3 +2,26 @@
 um.FemaleStudents();
 um.SortStudentsByAge();
 um.AllStudentsFromNltu();
+
+int[] numbers = { 1, 5, 3, 2, 0, 10, 15, 8 };
+
+var sortedNumbers = from number in numbers orderby number select number;
+System.Console.WriteLine("Sorted numbers:");
+foreach (var number in sortedNumbers)
+{
+    System.Console.WriteLine(number);
+}
+
+var reversedNumbers = sortedNumbers.Reverse();
+System.Console.WriteLine("Reversed numbers:");
+foreach (var number in reversedNumbers)
+{
+    System.Console.WriteLine(number);
+}
+
+var sortedByDescendingNumbers = from number in numbers orderby number descending select number;
+System.Console.WriteLine("Sorted by descending numbers:");
+foreach (var number in sortedByDescendingNumbers)
+{
+    System.Console.WriteLine(number);
+}
